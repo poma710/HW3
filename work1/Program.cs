76111883600d4501474a8113420bf8ed02853013
Roman <poma710@yandex.run>
2023-04-17 12:01:51 +0300

    Done

---
+++ work1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Палиндром. Точно!!!");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Это число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - число не является пятизначным");
+}
